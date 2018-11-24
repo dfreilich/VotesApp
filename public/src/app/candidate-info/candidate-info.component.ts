78e022a68986a7f638049f82ca6579902ae13055
shikharmathur <shikharmathur96@gmail.com>
2018-11-23 19:05:44 -0800

    fix next candidate page

--- public/src/app/candidate-info/candidate-info.component.ts
+++ public/src/app/candidate-info/candidate-info.component.ts
@@ -9,34 +9,33 @@ import { Routes, RouterModule, Router, ActivatedRoute } from "@angular/router";
 })
 export class CandidateInfoComponent implements OnInit {
 
-   candidate; curInfo;candidateInfo;
+   candidateID;candidate; curInfo;candidateInfo;
    constructor(private route: ActivatedRoute,
               private router: Router) {
     this.route.params.subscribe(params => {
-      this.candidate = params;
-      console.log(this.candidate);
+      this.candidateID = params;
     });
      this.candidateInfo = {
        1: {
-         name: 'Candidate 1', pos: 'Director of CMU', bio: 'Bio of Candidate 1', endorsements: 'Endorsements of Candidate 1', prevPos: 'Previous positions of Candidate 1'
+         id: 1, name: 'Candidate 1', pos: 'Director of CMU', bio: 'Bio of Candidate 1', endorsements: 'Endorsements of Candidate 1', prevPos: 'Previous positions of Candidate 1'
        },
        2: {
-         name: 'Candidate 2', pos: 'Director of CMU', bio: 'Bio of Candidate 2', endorsements: 'Endorsements of Candidate 2', prevPos: 'Previous positions of Candidate 2'
+         id:2, name: 'Candidate 2', pos: 'Director of CMU', bio: 'Bio of Candidate 2', endorsements: 'Endorsements of Candidate 2', prevPos: 'Previous positions of Candidate 2'
        },
        3: {
-         name: 'Candidate 3', pos: 'Director of CMU', bio: 'Bio of Candidate 3', endorsements: 'Endorsements of Candidate 3', prevPos: 'Previous positions of Candidate 3'
+         id:3, name: 'Candidate 3', pos: 'Director of CMU', bio: 'Bio of Candidate 3', endorsements: 'Endorsements of Candidate 3', prevPos: 'Previous positions of Candidate 3'
        },
        4: {
-         name: 'Candidate 4', pos: 'Director of CMU', bio: 'Bio of Candidate 4', endorsements: 'Endorsements of Candidate 4', prevPos: 'Previous positions of Candidate 4'
+         id:4, name: 'Candidate 4', pos: 'Director of CMU', bio: 'Bio of Candidate 4', endorsements: 'Endorsements of Candidate 4', prevPos: 'Previous positions of Candidate 4'
        },
        5: {
-         name: 'Candidate 5', pos: 'Director of CMU', bio: 'Bio of Candidate 5', endorsements: 'Endorsements of Candidate 5', prevPos: 'Previous positions of Candidate 5'
+         id:5, name: 'Candidate 5', pos: 'Director of CMU', bio: 'Bio of Candidate 5', endorsements: 'Endorsements of Candidate 5', prevPos: 'Previous positions of Candidate 5'
        },
        6: {
-         name: 'Candidate 6', pos: 'Director of CMU', bio: 'Bio of Candidate 6', endorsements: 'Endorsements of Candidate 6', prevPos: 'Previous positions of Candidate 6'
+         id:6, name: 'Candidate 6', pos: 'Director of CMU', bio: 'Bio of Candidate 6', endorsements: 'Endorsements of Candidate 6', prevPos: 'Previous positions of Candidate 6'
        }
      };
-     this.curInfo = this.candidateInfo[this.candidate.id];
+     this.candidate = this.candidateInfo[this.candidateID.id];
   }
 
   ngOnInit() {
@@ -44,7 +43,7 @@ export class CandidateInfoComponent implements OnInit {
   }
   nextCandidate(candidate) {
      let newID = +candidate.id + 1;
-    this.curInfo = this.candidateInfo[newID]
+     this.candidate = this.candidateInfo[newID];
   }
 
 }
