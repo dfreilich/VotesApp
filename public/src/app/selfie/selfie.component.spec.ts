0672b1d6714971d7067c49ea1db887172ea527b9
David Freilich <freilich.david@gmail.com>
2018-11-26 11:33:09 -0800

    changing test for selfie component

--- public/src/app/selfie/selfie.component.spec.ts
+++ public/src/app/selfie/selfie.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelfieComponent } from './selfie.component';
+import { AppModule } from "../app.module";
 
 describe('SelfieComponent', () => {
   let component: SelfieComponent;
@@ -8,9 +9,8 @@ describe('SelfieComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SelfieComponent ]
+      imports:[AppModule]
     })
-    .compileComponents();
   }));
 
   beforeEach(() => {
