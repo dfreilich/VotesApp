e4daad71a0f97a5865329c23fbab0b98415ef799
Serhan Oztekin <serhan.oztekin@west.cmu.edu>
2018-11-27 17:35:15 -0800

    candidate image added

--- public/src/app/vote/vote.component.ts
+++ public/src/app/vote/vote.component.ts
@@ -22,31 +22,31 @@ export class VoteComponent implements OnInit {
     });
     let candidateInfo = {
        1: {
-         id: 1, name: 'Candidate 1', pos: 'Director of CMU', bio: 'Bio of Candidate 1', endorsements: 'Endorsements of Candidate 1', prevPos: 'Previous positions of Candidate 1'
+         id: 1, name: 'Candidate 1', pos: 'Director of CMU', bio: 'Bio of Candidate 1', endorsements: 'Endorsements of Candidate 1', prevPos: 'Previous positions of Candidate 1', src: 'assets/candidate.jpg'
        },
        2: {
-         id:2, name: 'Candidate 2', pos: 'Director of CMU', bio: 'Bio of Candidate 2', endorsements: 'Endorsements of Candidate 2', prevPos: 'Previous positions of Candidate 2'
+         id:2, name: 'Candidate 2', pos: 'Director of CMU', bio: 'Bio of Candidate 2', endorsements: 'Endorsements of Candidate 2', prevPos: 'Previous positions of Candidate 2', src: 'assets/candidate.jpg'
        },
        3: {
-         id:3, name: 'Candidate 3', pos: 'Director of CMU', bio: 'Bio of Candidate 3', endorsements: 'Endorsements of Candidate 3', prevPos: 'Previous positions of Candidate 3'
+         id:3, name: 'Candidate 3', pos: 'Director of CMU', bio: 'Bio of Candidate 3', endorsements: 'Endorsements of Candidate 3', prevPos: 'Previous positions of Candidate 3', src: 'assets/candidate.jpg'
        },
        4: {
-         id:4, name: 'Candidate 4', pos: 'Director of CMU', bio: 'Bio of Candidate 4', endorsements: 'Endorsements of Candidate 4', prevPos: 'Previous positions of Candidate 4'
+         id:4, name: 'Candidate 4', pos: 'Director of CMU', bio: 'Bio of Candidate 4', endorsements: 'Endorsements of Candidate 4', prevPos: 'Previous positions of Candidate 4', src: 'assets/candidate.jpg'
        },
        5: {
-         id:5, name: 'Candidate 5', pos: 'Director of CMU', bio: 'Bio of Candidate 5', endorsements: 'Endorsements of Candidate 5', prevPos: 'Previous positions of Candidate 5'
+         id:5, name: 'Candidate 5', pos: 'Director of CMU', bio: 'Bio of Candidate 5', endorsements: 'Endorsements of Candidate 5', prevPos: 'Previous positions of Candidate 5', src: 'assets/candidate.jpg'
        },
        6: {
-         id:6, name: 'Candidate 6', pos: 'Director of CMU', bio: 'Bio of Candidate 6', endorsements: 'Endorsements of Candidate 6', prevPos: 'Previous positions of Candidate 6'
+         id:6, name: 'Candidate 6', pos: 'Director of CMU', bio: 'Bio of Candidate 6', endorsements: 'Endorsements of Candidate 6', prevPos: 'Previous positions of Candidate 6', src: 'assets/vote2.png'
        },
        7: {
-         id:7, name: 'Candidate 7', pos: 'Director of CMU', bio: 'Bio of Candidate 7', endorsements: 'Endorsements of Candidate 7', prevPos: 'Previous positions of Candidate 7'
+         id:7, name: 'Candidate 7', pos: 'Director of CMU', bio: 'Bio of Candidate 7', endorsements: 'Endorsements of Candidate 7', prevPos: 'Previous positions of Candidate 7', src: 'assets/vote2.png'
        },
        8: {
-         id:8, name: 'Candidate 8', pos: 'Director of CMU', bio: 'Bio of Candidate 8', endorsements: 'Endorsements of Candidate 8', prevPos: 'Previous positions of Candidate 8'
+         id:8, name: 'Candidate 8', pos: 'Director of CMU', bio: 'Bio of Candidate 8', endorsements: 'Endorsements of Candidate 8', prevPos: 'Previous positions of Candidate 8', src: 'assets/vote2.png'
        },
        9: {
-         id:9, name: 'Candidate 9', pos: 'Director of CMU', bio: 'Bio of Candidate 9', endorsements: 'Endorsements of Candidate 9', prevPos: 'Previous positions of Candidate 9'
+         id:9, name: 'Candidate 9', pos: 'Director of CMU', bio: 'Bio of Candidate 9', endorsements: 'Endorsements of Candidate 9', prevPos: 'Previous positions of Candidate 9', src: 'assets/vote2.png'
        }
      };
      this.presidency = candidateInfo[this.presidencyid];
